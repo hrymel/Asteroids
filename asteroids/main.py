803ce65883a7f781dd74a03649e25adf4da77a4f
hrymel <hrymel@gmail.com>
2025-06-29 14:38:59 -0400

    added an on screen score sysstem

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,25 +1,73 @@
 import pygame
 from constants import *
-
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+import sys
 
 
 def main():
-	pygame.init()
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init()
+    pygame.font.init()
+    score = 0
+    score_increment = 10
 
-	running = True
-	while running:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				return
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-		screen.fill("black")
-		pygame.display.flip()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+    Shot.containers = (shots, updatable, drawable)
 
-	print("Starting Asteroids!")
-	print(f"Screen width: {constants.SCREEN_WIDTH}")
-	print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
+    while True:
+        # setting up the font object
+        font = pygame.font.Font(None, 36)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.doesCollide(shot):
+                    asteroid.split()
+                    asteroid.kill()
+                    shot.kill()
+                    score += score_increment
+
+            if player.doesCollide(asteroid):
+                sys.exit(f"Game over! Your score was {score}")
+                return
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        # Draw the score to the screen
+        score_text = font.render(f'Score: {score}', True, (255, 255, 255))
+        screen.blit(score_text, (10, 10))
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
